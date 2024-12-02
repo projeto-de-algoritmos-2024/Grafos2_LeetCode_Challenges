620489459904c727cac38a79984955019eba87e8
Yan Werlley <yanwerlley0101@gmail.com>
2024-12-02 18:52:02 -0300

    refactor(743): update code for graph
    Co-authored-by: SamuelGSouza <samuelgomes318@gmail.com>

--- 743/743.py
+++ 743/743.py
@@ -1,29 +1,32 @@
 import heapq
+from collections import defaultdict
 
 class Solution:
     def networkDelayTime(self, times, n, k):
-        graph = {i: [] for i in range(1, n+1)}
+
+        # Construcão do grafo
+        graph = defaultdict(list)
         for u, v, w in times:
             graph[u].append((v, w))
 
-        pq = [(0, k)] 
-        distances = {i: float('inf') for i in range(1, n+1)}
+        # Fila de prioridade
+        pq = [(0, k)]
+        distances = {i: float('inf') for i in range(1, n+1)} 
         distances[k] = 0
-        visited = set() 
 
         while pq:
             current_dist, node = heapq.heappop(pq)
 
-            if node in visited:
+            if current_dist > distances[node]:
                 continue
-            visited.add(node)
 
+            # Atualiza vizinhos
             for neighbor, weight in graph[node]:
-                if neighbor not in visited:
-                    new_dist = current_dist + weight
-                    if new_dist < distances[neighbor]:
-                        distances[neighbor] = new_dist
-                        heapq.heappush(pq, (new_dist, neighbor))
+                new_dist = current_dist + weight
+                if new_dist < distances[neighbor]:
+                    distances[neighbor] = new_dist
+                    heapq.heappush(pq, (new_dist, neighbor))
 
+        # Verifica a maior distânci
         max_distance = max(distances.values())
         return max_distance if max_distance < float('inf') else -1
